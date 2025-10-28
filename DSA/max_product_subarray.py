e08a6adaddbbe2c97c485e0b39ebc3881e7ec5b6
Riya karmakar <riyakarmakar.kolkata@gmail.com>
2025-10-28 22:25:13 +0530

    added new files

---
+++ DSA/max_product_subarray.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = max(nums)
+        curMax, curMin = 1, 1
+        for num in nums:
+            tmp = num * curMax
+            curMax = max(num*curMax, num*curMin, num)
+            curMin = min(tmp, num*curMin, num)
+            res = max(res, curMax)
+        return res
+        
+if __name__ == "__main__":
+    nums = list(map(int, input("Enter numbers to check max pproduct of subarray, separated by space: ").split()))
+    sol = Solution()
+    res = sol.maxProduct(nums)
+    print("Maximum product of subarray : ", res)
